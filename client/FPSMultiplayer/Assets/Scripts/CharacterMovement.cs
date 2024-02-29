0ad160d822a4ca2af17b096b6068b24ecaa67fae
scrawach <scrawach@gmail.com>
2024-02-29 11:30:50 +0300

    added player prefab with player input and character movement components

---
+++ client/FPSMultiplayer/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1f;
+    
+    public void Move(Vector2 direction)
+    {
+        var timeStep = _speed * Time.deltaTime;
+        transform.position += timeStep * new Vector3(direction.x, 0, direction.y);
+    }
+}
