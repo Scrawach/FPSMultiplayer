addf4c85e2850eca9a7a0a9c0a26089a62ee5cca
scrawach <scrawach@gmail.com>
2024-03-01 22:49:01 +0300

    implemented naive enemy position prediction

--- client/FPSMultiplayer/Assets/Scripts/Gameplay/EnemyController.cs
+++ client/FPSMultiplayer/Assets/Scripts/Gameplay/EnemyController.cs
@@ -8,7 +8,36 @@ namespace Gameplay
     {
         [SerializeField] private CharacterMovement _movement;
 
-        public void OnPositionChanged(Position current, Position previous) => 
-            transform.position = current.ToVector3();
+        private bool _mustInitPosition;
+        private Vector3 _initPosition;
+        private Vector2 _predictedDirection;
+        
+        public void OnPositionChanged(Position current, Position previous)
+        {
+            _mustInitPosition = true;
+            _initPosition = current.ToVector3();
+            _predictedDirection = PredictMovementDirection(current, previous);
+        }
+
+        private static Vector2 PredictMovementDirection(Position current, Position previous)
+        {
+            if (previous is null)
+                return Vector2.zero;
+
+            return current.ToVector2() - previous.ToVector2();
+        }
+        
+        private void Update()
+        {
+            if (_mustInitPosition)
+            {
+                transform.position = _initPosition;
+                _mustInitPosition = false;
+            }
+            else
+            {
+                _movement.Move(_predictedDirection.normalized);
+            }
+        }
     }
 }
