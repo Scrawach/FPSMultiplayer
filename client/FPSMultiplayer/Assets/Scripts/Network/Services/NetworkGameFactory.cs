f472aeed4dcccf69311e1265823c8c4e4a90eb44
scrawach <scrawach@gmail.com>
2024-03-05 10:04:15 +0300

    moved creation code from network players builder to network game factory

--- client/FPSMultiplayer/Assets/Scripts/Network/Services/NetworkGameFactory.cs
+++ client/FPSMultiplayer/Assets/Scripts/Network/Services/NetworkGameFactory.cs
@@ -1,4 +1,5 @@
-﻿using Extensions;
+﻿using System.Collections.Generic;
+using Extensions;
 using Gameplay;
 using Network.Schemas;
 using Services;
@@ -8,15 +9,36 @@ namespace Network.Services
 {
     public class NetworkGameFactory
     {
+        private readonly NetworkManager _network;
         private readonly GameFactory _factory;
+        private readonly Dictionary<string, GameObject> _instances;
 
-        public NetworkGameFactory(GameFactory factory) => 
+        public NetworkGameFactory(NetworkManager network, GameFactory factory)
+        {
+            _network = network;
             _factory = factory;
+            _instances = new Dictionary<string, GameObject>();
+        }
+        
+        public GameObject CreateUnit(string key, Player state)
+        {
+            _instances[key] = key == _network.Id
+                ? CreatePlayer(state)
+                : CreateEnemy(state);
+            return _instances[key];
+        }
 
-        public GameObject CreatePlayer(Player state) => 
+        public void Destroy(string key)
+        {
+            var instance = _instances[key];
+            _instances.Remove(key);
+            Object.Destroy(instance);
+        }
+
+        private GameObject CreatePlayer(Player state) => 
             _factory.CreatePlayer(state.position.ToVector3());
 
-        public GameObject CreateEnemy(Player state)
+        private GameObject CreateEnemy(Player state)
         {
             var enemy = _factory.CreateEnemy(state.position.ToVector3());
             var enemyController = enemy.GetComponent<EnemyMovement>();
--- client/FPSMultiplayer/Assets/Scripts/Network/Services/StateListeners/NetworkPlayersBuilder.cs
+++ client/FPSMultiplayer/Assets/Scripts/Network/Services/StateListeners/NetworkPlayersBuilder.cs
@@ -1,23 +1,14 @@
-using System.Collections.Generic;
 using Network.Schemas;
-using UnityEngine;
 
 namespace Network.Services.StateListeners
 {
     public class NetworkPlayersBuilder : INetworkStateListener
     {
-        private readonly NetworkManager _network;
         private readonly NetworkGameFactory _factory;
 
-        private readonly Dictionary<string, GameObject> _instances;
-
-        public NetworkPlayersBuilder(NetworkManager network, NetworkGameFactory factory)
-        {
-            _network = network;
+        public NetworkPlayersBuilder(NetworkGameFactory factory) => 
             _factory = factory;
-            _instances = new Dictionary<string, GameObject>();
-        }
-    
+
         public void ChangeState(State state, bool isFirstState)
         {
             if (isFirstState == false)
@@ -28,18 +19,9 @@ namespace Network.Services.StateListeners
         }
         
         private void OnPlayerAdded(string key, Player player) => 
-            _instances[key] = CreateUnit(key, player);
+            _factory.CreateUnit(key, player);
 
-        private void OnPlayerRemoved(string key, Player value)
-        {
-            var instance = _instances[key];
-            _instances.Remove(key);
-            Object.Destroy(instance);
-        }
-        
-        private GameObject CreateUnit(string key, Player player) =>
-            _network.Id == key 
-                ? _factory.CreatePlayer(player) 
-                : _factory.CreateEnemy(player);
+        private void OnPlayerRemoved(string key, Player value) => 
+            _factory.Destroy(key);
     }
 }
