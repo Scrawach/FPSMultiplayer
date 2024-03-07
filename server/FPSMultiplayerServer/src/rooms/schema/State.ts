8afcc169b4581c6a4e113cefc74dabdc6aac4d44
scrawach <scrawach@gmail.com>
2024-03-08 00:25:03 +0300

    fixed bug on server, removed old position class references

--- server/FPSMultiplayerServer/src/rooms/schema/State.ts
+++ server/FPSMultiplayerServer/src/rooms/schema/State.ts
@@ -1,6 +1,6 @@
 import { Schema, MapSchema, type } from "@colyseus/schema";
 import { Player } from "./Player";
-import { Position } from "./Position"
+import { Vector3Data } from "./Vector3Data"
   
 export class State extends Schema {
     @type({ map: Player }) players = new MapSchema<Player>();
@@ -13,7 +13,7 @@ export class State extends Schema {
         this.players.delete(sessionId);
     }
 
-    setPlayerPosition(sessionId: string, position: Position) {
+    setPlayerPosition(sessionId: string, position: Vector3Data) {
         if (position) {
             this.players.get(sessionId).position = position;
         }
