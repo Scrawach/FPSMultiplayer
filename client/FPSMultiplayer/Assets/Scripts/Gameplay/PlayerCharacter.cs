95eb3bcc21cf2427dcd38c3817ca301444ce16f4
scrawach <scrawach@gmail.com>
2024-03-08 20:01:53 +0300

    added tranform input relative by player body

--- client/FPSMultiplayer/Assets/Scripts/Gameplay/PlayerCharacter.cs
+++ client/FPSMultiplayer/Assets/Scripts/Gameplay/PlayerCharacter.cs
@@ -28,8 +28,14 @@ namespace Gameplay
 
         private void MoveTo(Vector3 direction)
         {
-            var nextPosition = transform.position + direction;
+            var nextPosition = transform.position + TransformInputRelativeToBody(direction);
             _movement.MoveTo(nextPosition);
+        }
+
+        private Vector3 TransformInputRelativeToBody(Vector3 input)
+        {
+            var playerTransform = transform;
+            return playerTransform.forward * input.z + playerTransform.right * input.x;
         }
 
         private void RotateTo(Vector2 mouseAxis)
