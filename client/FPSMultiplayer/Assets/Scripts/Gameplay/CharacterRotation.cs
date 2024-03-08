f8fb8519d0016e805f3b360ab32fa2175f62dd45
scrawach <scrawach@gmail.com>
2024-03-08 18:44:40 +0300

    added limitation for head rotation

--- client/FPSMultiplayer/Assets/Scripts/Gameplay/CharacterRotation.cs
+++ client/FPSMultiplayer/Assets/Scripts/Gameplay/CharacterRotation.cs
@@ -5,10 +5,15 @@ namespace Gameplay
     public class CharacterRotation : MonoBehaviour
     {
         [SerializeField] private Transform _head;
+        [SerializeField] private float _minAngleX = -90f;
+        [SerializeField] private float _maxAngleX = 90f;
 
+        private float _currentRotateX;
+        
         public void RotateX(float angle)
         {
-            _head.Rotate(angle, 0, 0);
+            _currentRotateX = Mathf.Clamp(_currentRotateX + angle, _minAngleX, _maxAngleX);
+            _head.transform.localEulerAngles = new Vector3(_currentRotateX, 0, 0);
         }
 
         public void RotateY(float angle)
