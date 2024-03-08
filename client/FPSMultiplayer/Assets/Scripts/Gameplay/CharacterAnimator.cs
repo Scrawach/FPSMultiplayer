7a76874035512fd9a77d0ff7d8b4421583b46577
scrawach <scrawach@gmail.com>
2024-03-08 20:30:13 +0300

    fixed horizontal animation moving

--- client/FPSMultiplayer/Assets/Scripts/Gameplay/CharacterAnimator.cs
+++ client/FPSMultiplayer/Assets/Scripts/Gameplay/CharacterAnimator.cs
@@ -16,6 +16,9 @@ namespace Gameplay
             var localVelocity = _character.transform.InverseTransformVector(_character.velocity);
             var clampedSpeed = localVelocity.magnitude / _movement.Speed;
             var sign = Mathf.Sign(localVelocity.z);
+
+            if (Mathf.Abs(localVelocity.z) < 0.1f)
+                sign = 1;
             
             _animator.SetFloat(Speed, sign * clampedSpeed);
             _animator.SetBool(GroundedHash, !_movement.IsJumping);
