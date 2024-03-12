e7984ce062a89e61d5e91cb78cb67313d593b79c
scrawach <scrawach@gmail.com>
2024-03-12 14:34:37 +0300

    added private modifier for health setters

--- client/FPSMultiplayer/Assets/Scripts/Gameplay/Health.cs
+++ client/FPSMultiplayer/Assets/Scripts/Gameplay/Health.cs
@@ -7,9 +7,9 @@ namespace Gameplay
     {
         public event Action Changed;
         
-        [field: SerializeField] public int Total { get; set; }
+        [field: SerializeField] public int Total { get; private set; }
         
-        [field: SerializeField] public int Current { get; set; }
+        [field: SerializeField] public int Current { get; private set; }
 
         public float Ratio => (float) Current / Total;
         
