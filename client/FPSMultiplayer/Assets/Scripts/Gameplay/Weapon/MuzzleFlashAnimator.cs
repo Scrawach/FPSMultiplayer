129034422c22a32e0744b21d85f9a9ce6e7c47c2
scrawach <scrawach@gmail.com>
2024-03-19 18:50:59 +0300

    added muzzle flash to pistol and gun

---
+++ client/FPSMultiplayer/Assets/Scripts/Gameplay/Weapon/MuzzleFlashAnimator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Weapon
+{
+    public class MuzzleFlashAnimator : MonoBehaviour
+    {
+        private static readonly int FireHash = Animator.StringToHash("Fire");
+
+        [SerializeField] private Gun _gun;
+        [SerializeField] private Animator _animator;
+
+        private void OnEnable() => 
+            _gun.Fired += OnFired;
+
+        private void OnDisable() => 
+            _gun.Fired -= OnFired;
+
+        private void OnFired()
+        {
+            _animator.SetTrigger(FireHash);
+        }
+    }
+}
